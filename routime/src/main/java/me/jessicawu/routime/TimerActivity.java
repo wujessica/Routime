b4eae9bf3878b666189f4cb7c1139b6624a08def
Scott So <scotthhso@gmail.com>
2014-05-22 21:04:59 -0400

    added pause, reset and continue feature

--- routime/src/main/java/me/jessicawu/routime/TimerActivity.java
+++ routime/src/main/java/me/jessicawu/routime/TimerActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import java.text.DecimalFormat;
 
 /**
- * Created by jessica on 19/05/14.
+ * Created by jessica and scott on 19/05/14.
  */
 public class TimerActivity extends Activity implements View.OnClickListener {
     private CountDownTimer countDownTimer;
@@ -24,7 +24,7 @@ public class TimerActivity extends Activity implements View.OnClickListener {
     private long startTime = 0;
     private final long interval = 5;
     private String exerciseName = "";
-
+    double time;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,24 +38,35 @@ public class TimerActivity extends Activity implements View.OnClickListener {
         exerciseNameTV = (TextView) this.findViewById(R.id.current_exercise);
         exerciseNameTV.setText(exerciseName);
 
-        startB = (Button) this.findViewById(R.id.button);
+        startB = (Button) this.findViewById(R.id.start);
         startB.setOnClickListener(this);
         timeLeft = (TextView) this.findViewById(R.id.timer);
         countDownTimer = new MyCountDownTimer(startTime, interval);
-        timeLeft.setText(timeLeft.getText() + String.valueOf(startTime / 1000) + ".000");
+        timeLeft.setText(timeLeft.getText() + String.valueOf(startTime / 1000) + ".00");
     }
 
     @Override
     public void onClick(View v) {
         if(!timerHasStarted) {
+
             countDownTimer.start();
             timerHasStarted = true;
             startB.setText("PAUSE");
+
         } else {
+
             countDownTimer.cancel();
+            countDownTimer = new MyCountDownTimer((long)time, interval);
+
             timerHasStarted = false;
             startB.setText("CONTINUE");
         }
+    }
+
+    public void restart(View v) {
+        countDownTimer.cancel();
+        countDownTimer = new MyCountDownTimer(startTime, interval);
+        timeLeft.setText(String.valueOf(startTime / 1000) + ".00");
     }
 
     public class MyCountDownTimer extends CountDownTimer {
@@ -72,7 +83,7 @@ public class TimerActivity extends Activity implements View.OnClickListener {
         @Override
         public void onTick(long millisUntilFinished){
             //timeLeft.setText("" + millisUntilFinished / 1000 + "." + millisUntilFinished % 1000);
-            double time = (double) millisUntilFinished;
+             time = (double) millisUntilFinished;
             DecimalFormat df = new DecimalFormat("0.00");
             timeLeft.setText(df.format(time/1000));
         }
