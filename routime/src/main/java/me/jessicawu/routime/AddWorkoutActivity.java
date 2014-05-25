8d31ef09070249457d0db295a4848b92576a3faa
Scott So <scotthhso@gmail.com>
2014-05-25 10:43:00 -0400

    created addworkoutactivity

---
+++ routime/src/main/java/me/jessicawu/routime/AddWorkoutActivity.java
@@ -0,0 +1,21 @@
+package me.jessicawu.routime;
+
+/**
+ * Created by scottso on 2014-05-25.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AddWorkoutActivity extends Activity  {
+
+
+
+
+
+}
