657f4975b593974ac59f0f9e11dbd390286ea36f
Scott So <scotthhso@gmail.com>
2014-05-29 23:52:12 -0400

    created a list view

---
+++ routime/src/main/java/me/jessicawu/routime/ListExcercisesActivity.java
@@ -0,0 +1,18 @@
+package me.jessicawu.routime;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by scottso on 2014-05-29.
+ */
+public class ListExcercisesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addworkout);
+    }
+
+
+}
