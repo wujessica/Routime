e997386f672674ba9cc60a7e8f69ff269e211a41
Scott So <scotthhso@gmail.com>
2014-06-01 11:13:53 -0400

    updated to a better working version

--- routime/src/main/java/me/jessicawu/routime/ListExcercisesActivity.java
+++ routime/src/main/java/me/jessicawu/routime/ListExcercisesActivity.java
@@ -2,7 +2,9 @@ package me.jessicawu.routime;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,69 +14,61 @@ import java.util.ArrayList;
  */
 public class ListExcercisesActivity extends Activity {
 
+
+    ListView listView ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addworkout);
-    }
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.routineList);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
 
-        final ListView listview = (ListView) findViewById(R.id.listview);
-        String[] values = new String[] {  };
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
 
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
-        final StableArrayAdapter adapter = new StableArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
-        listview.setAdapter(adapter);
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
+            public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                final String item = (String) parent.getItemAtPosition(position);
-                view.animate().setDuration(2000).alpha(0)
-                        .withEndAction(new Runnable() {
-                            @Override
-                            public void run() {
-                                list.remove(item);
-                                adapter.notifyDataSetChanged();
-                                view.setAlpha(1);
-                            }
-                        });
-            });
 
+                // ListView Clicked item index
+                int itemPosition     = position;
 
-        private class StableArrayAdapter extends ArrayAdapter<String> {
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
 
-            HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+                
 
-            public StableArrayAdapter(Context context, int textViewResourceId,
-                                      List<String> objects) {
-                super(context, textViewResourceId, objects);
-                for (int i = 0; i < objects.size(); ++i) {
-                    mIdMap.put(objects.get(i), i);
-                }
             }
 
-            @Override
-            public long getItemId(int position) {
-                String item = getItem(position);
-                return mIdMap.get(item);
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return true;
-            }
-
-        }
-
+        });
     }
-}
+
 
 
 }
