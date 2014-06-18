1f7148c5b518e8fcd5eacb2153f69137ae0fed3a
wujessica <jessica.wu022@gmail.com>
2014-06-17 22:58:12 -0400

    Displays one exercise and time inputted in the ListView.

---
+++ routime/src/main/java/me/jessicawu/routime/ListExercisesActivity.java
@@ -0,0 +1,73 @@
+package me.jessicawu.routime;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by scottso on 2014-05-29.
+ */
+public class ListExercisesActivity extends Activity {
+
+
+    ListView  listView;
+
+    ArrayList<String> workout;
+
+    ArrayAdapter<String> adapter;
+
+    private boolean first = true;
+
+    public void updateList() {
+        Intent intent = getIntent();
+
+        String exercise = intent.getStringExtra(AddWorkoutActivity.LIST_EXERCISE);
+        String time = intent.getStringExtra(AddWorkoutActivity.LIST_TIMER_AMOUNT);
+
+        workout.add(exercise);
+        workout.add(time);
+
+        adapter.notifyDataSetChanged();
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//
+//                // ListView Clicked item index
+//                int itemPosition  = position;
+//
+//                // ListView Clicked item value
+//                String  itemValue    = (String) listView.getItemAtPosition(position);
+//            }
+//        });
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addworkout);
+
+        if (first) {
+            // Get ListView object from xml
+            listView = (ListView) findViewById(R.id.routineList);
+            workout = new ArrayList<String>();
+            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, workout);
+            first = false;
+        }
+
+        updateList();
+    }
+}
