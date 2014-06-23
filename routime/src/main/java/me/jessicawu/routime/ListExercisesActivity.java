e53a310a90e9ceddfe33b84f3d3a21035827d6d1
Scott So <scotthhso@gmail.com>
2014-06-22 21:57:59 -0400

    added bundle

--- routime/src/main/java/me/jessicawu/routime/ListExercisesActivity.java
+++ routime/src/main/java/me/jessicawu/routime/ListExercisesActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -58,7 +57,7 @@ public class ListExercisesActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addworkout);
+        setContentView(R.layout.fragment_addworkout);
 
         if (first) {
             // Get ListView object from xml
