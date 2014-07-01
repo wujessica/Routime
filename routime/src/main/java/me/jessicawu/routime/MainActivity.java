a28e3e9eb4b69c26858da01a23a0f04271a22736
wujessica <jessica.wu022@gmail.com>
2014-07-01 18:22:18 -0400

    Rebased development-list on top of development.

--- routime/src/main/java/me/jessicawu/routime/MainActivity.java
+++ routime/src/main/java/me/jessicawu/routime/MainActivity.java
@@ -1,8 +1,8 @@
 package me.jessicawu.routime;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
