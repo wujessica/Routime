3012f9972cc6fdaa39590f242b7f931ad3dc15a8
Scott So <scotthhso@gmail.com>
2014-07-06 20:28:26 -0400

    matched the style to all the other activities

--- routime/src/main/java/me/jessicawu/routime/ListExercisesActivity.java
+++ routime/src/main/java/me/jessicawu/routime/ListExercisesActivity.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
 * Created by scottso on 2014-05-29.
 */
@@ -25,6 +27,7 @@ public class ListExercisesActivity extends Activity implements AddWorkoutDialog.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault("fonts/SourceSansPro-Black.ttf", R.attr.fontPath);
         setContentView(R.layout.activity_listexercises);
 
         Button button = (Button) findViewById(R.id.add);
@@ -34,6 +37,8 @@ public class ListExercisesActivity extends Activity implements AddWorkoutDialog.
                 onAdd();
             }
         });
+
+
 
         listView = (ListView) findViewById(R.id.exercise_list);
         workout = new ArrayList<ListExercisesItem>();
