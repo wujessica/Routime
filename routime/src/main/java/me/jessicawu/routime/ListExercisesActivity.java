f928cac8407ca1535ff65a695460836c083622ef
wujessica <jessica.wu022@gmail.com>
2014-12-23 18:36:53 -0500

    Update to AS 1.0.1 ugh

--- routime/src/main/java/me/jessicawu/routime/ListExercisesActivity.java
+++ routime/src/main/java/me/jessicawu/routime/ListExercisesActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
